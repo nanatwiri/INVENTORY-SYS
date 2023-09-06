f6303f82dd2423d6f049b97293102b7614ac5f73
kanana00-00 <victoria.mutwiri@strathmore.edu>
2023-09-06 21:48:55 +0300

    Tuesday updates

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from .views import TestCRUD
+
 import debug_toolbar
 
 
 urlpatterns = [
-    path('__debug__/', include('debug_toolbar.urls')),  # Fixed quotes
-    path('add_inventory/<int:item_id>/', views.add_item, name='add_item'),
-    path('', views.item_list, name='item_list'),
-    path('edit/<int:pk>/', views.edit_item, name='edit_item'),
-    path('delete/<int:pk>/', views.delete_item, name='delete_item'),
-    path('test_crud/', TestCRUD.as_view(), name='test_crud'),
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('', views.Inventory, name='Home'),
+    path('add_items/', views.add_items, name='add_items'),
+    path('edit_item/', views.edit_item, name='edit_item'),
+    path('item_list/', views.item_list, name='item_list'),
+
 ]
